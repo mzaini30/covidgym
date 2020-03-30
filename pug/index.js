ac02565f3245d5f8e67b20e816885ff883ad6d8e
mzaini30 <muhzaini30@gmail.com>
2020-03-30 22:33:49 +0800

    upload

--- pug/index.js
+++ pug/index.js
@@ -20,3 +20,22 @@ var interval = setInterval(() => {
   berulang == 10 ? clearInterval(interval) : ""
   $(".selisih").html((Number($(".kemarin").html().replace(/\./g, "")) - Number($(".kemarin-lusa").html().replace(/\./g, ""))).toLocaleString("id"))
 }, 1000)
+
+!localStorage.latihan ? localStorage.setItem("latihan", 0) : ""
+var tampil_latihan = () => $(".latihan").html(localStorage.latihan)
+tampil_latihan()
+$(".submit-latihan").on("submit", x => {
+  x.preventDefault()
+  var latihan_sebelumnya = Number(localStorage.latihan)
+  var latihan_baru = latihan_sebelumnya + Number($(".tambah-latihan").val())
+  localStorage.setItem("latihan", latihan_baru)
+  tampil_latihan()
+  $(".tambah-latihan").val("")
+})
+$(".reset").click(() => {
+  var hapus = confirm("Reset kah?")
+  if (hapus){
+    localStorage.setItem("latihan", 0)
+    tampil_latihan()
+  }
+})
